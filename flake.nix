c35a14a64b9d3302867fed31d71d7515f6c659c2
Po-Yeh Chen <poyehchen@cs.nctu.edu.tw>
2023-05-11 23:46:16 +0800

    Remove architectures other than x86_64.

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
       imports = [
         inputs.devenv.flakeModule
       ];
-      systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      systems = [ "x86_64-linux" "x86_64-darwin" ];
 
       perSystem = { config, self', inputs', pkgs, system, ... }:
         let
